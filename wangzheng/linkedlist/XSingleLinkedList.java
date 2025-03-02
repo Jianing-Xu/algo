d2c4f78d94f917e292e548af44a46f8c7c1e7912
xjn <240390931@qq.com>
2025-03-02 23:38:59 +0800

    feat: linkedlist

--- wangzheng/linkedlist/XSingleLinkedList.java
+++ wangzheng/linkedlist/XSingleLinkedList.java
@@ -1,9 +1,59 @@
 package linkedlist;
 
+import utils.ListNode;
+import utils.PrintUtil;
+
 // 单链表
 public class XSingleLinkedList {
 
-    public static void main(String[] args) {
+    public ListNode head;
 
+    public XSingleLinkedList() {
+        this.head = new ListNode(0);
+    }
+
+    public void insertToHead(int value) {
+        ListNode node = new ListNode(value);
+        node.next = head.next;
+        head.next = node;
+    }
+
+    public void insertTail(int value) {
+        // 可以增加尾节点优化
+        ListNode node = new ListNode(value);
+        ListNode cur = head;
+        while (cur.next != null) {
+            cur = cur.next;
+        }
+        cur.next = node;
+    }
+
+    public void delete(int value) {
+        ListNode cur = head;
+        while (cur.next != null) {
+            if (cur.next.val == value) {
+                cur.next = cur.next.next;
+            } else {
+                cur = cur.next;
+            }
+        }
+    }
+
+    public ListNode findByValue(int value) {
+        ListNode cur = head;
+        while (cur.next != null && cur.next.val != value) {
+            cur.next = cur.next.next;
+        }
+        return cur;
+    }
+
+    public static void main(String[] args) {
+        XSingleLinkedList xSingleLinkedList = new XSingleLinkedList();
+        xSingleLinkedList.insertToHead(1);
+        xSingleLinkedList.insertToHead(2);
+        xSingleLinkedList.insertToHead(3);
+        PrintUtil.printLinkedList(xSingleLinkedList.head);
+        xSingleLinkedList.delete(2);
+        PrintUtil.printLinkedList(xSingleLinkedList.head);
     }
 }
