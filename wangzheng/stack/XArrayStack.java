833bf1d453c4e81e9762ddd19012be349d29a661
xujianing <xujianing@scmttec.com>
2025-03-04 10:24:28 +0800

    feat: stack

--- wangzheng/stack/XArrayStack.java
+++ wangzheng/stack/XArrayStack.java
@@ -1,4 +1,46 @@
 package stack;
 
+import utils.PrintUtil;
+
+// 用数组实现一个顺序栈
 public class XArrayStack {
+
+    int[] data;
+    int size;
+
+    public XArrayStack(int capacity) {
+        this.data = new int[capacity];
+        this.size = 0;
+    }
+
+    public void push(int value) {
+        if (size == data.length) {
+            System.out.println("Stack is full");
+            return;
+        }
+        data[size++] = value;
+    }
+
+    public int pop() {
+        if (size == 0) {
+            System.out.println("Stack is empty");
+            return -1;
+        }
+        int datum = data[size - 1];
+        data[--size] = 0;
+        return datum;
+    }
+
+    public static void main(String[] args) {
+        XArrayStack stack = new XArrayStack(3);
+        stack.push(1);
+        stack.push(3);
+        stack.push(5);
+        stack.push(7);
+        PrintUtil.printArray(stack.data);
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        PrintUtil.printArray(stack.data);
+    }
 }
--- wangzheng/stack/XLinkedStack.java
+++ wangzheng/stack/XLinkedStack.java
@@ -1,4 +1,49 @@
 package stack;
 
+import utils.ListNode;
+import utils.PrintUtil;
+
+// 用链表实现一个链式栈
 public class XLinkedStack {
+
+    ListNode top;
+    int size;
+
+    public void push(int value) {
+        if (top == null) {
+            top = new ListNode(value);
+        } else {
+            ListNode node = new ListNode(value);
+            node.next = top;
+            top = node;
+        }
+        size++;
+    }
+
+    public int pop() {
+        if (top != null) {
+            int value = top.val;
+            top = top.next;
+            size--;
+            return value;
+        } else {
+            System.out.println("Stack is empty");
+            return -1;
+        }
+    }
+
+    public void printStack() {
+        PrintUtil.printLinkedList(top);
+    }
+
+    public static void main(String[] args) {
+        XLinkedStack stack = new XLinkedStack();
+        stack.push(1);
+        stack.push(3);
+        stack.printStack();
+        int pop = stack.pop();
+        System.out.println(pop);
+        stack.push(5);
+        stack.printStack();
+    }
 }
