8264c3c22417811170544d8bdd8e37b91a83e3a9
xujianing <xujianing@scmttec.com>
2025-03-13 14:06:37 +0800

    feat: graph

--- wangzheng/graph/XGraph.java
+++ wangzheng/graph/XGraph.java
@@ -1,5 +1,22 @@
 package graph;
 
+import java.util.LinkedList;
+
 public class XGraph {
 
+    int v; // No. of vertices
+    LinkedList<Integer> adj[]; // Adjacency Lists
+
+    XGraph(int v) {
+        this.v = v;
+        adj = new LinkedList[v];
+        for (int i = 0; i < v; ++i)
+            adj[i] = new LinkedList();
+    }
+
+    public void addEdge(int v, int w) {
+        // 无向图一条边存两次
+        adj[v].add(w);
+        adj[w].add(v);
+    }
 }
